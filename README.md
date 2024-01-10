e2fbc036a9c2fb059d7cb8b3ee4b5fa6bae3b0df
anqisoft <anqisoft@gmail.com>
2024-01-10 23:03:39 +0800

    Update the comments and readme.md

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # js_node_cn2tw
-&lt;en_us>Translate file content from Simplified Chinese to Traditional Chinese.&lt;/en_us>&lt;zh_cn>翻译文件中的简体中文到繁体中文。&lt;/zh_cn>&lt;zh_tw>翻譯文件中的簡體中文到繁體中文。&lt;/zh_tw>
+
+### Usage:
+```bash
+npm i -g @dishanqian/cn2tw
+cn2tw zh_cn.txt zh_tw.txt
+```
+
+### Usage:
+```bash
+git clone git+ssh://git@github.com/anqisoft/js_node_cn2tw
+cd js_node_cn2tw
+npm i @dishanqian/cn_and_tw
+node index.js zh_cn.txt zh_tw.txt
+```
+
+## <en_us>
+Created on Wed Jan 10 2024 20:09:00<br>
+Feature: Translate file content from Simplified Chinese to Traditional Chinese.<br>
+</en_us>
+
+## <zh_cn>
+创建：2024年1月10日 20:09:00<br>
+功能：翻译文件中的简体中文到繁体中文。<br>
+</zh_cn>
+
+## <zh_tw>
+創建：2024年1月10日 20:09:00<br>
+功能：翻譯文件中的簡體中文到繁體中文。<br>
+</zh_tw>
--- index.js
+++ index.js
@@ -1,16 +1,20 @@
+#!/usr/bin/env node
 "use strict";
 /*
  * Copyright (c) 2024 anqisoft@gmail.com
  * index.ts
  *
- * Usage: ```
+ * Usage: ```bash
 npm i -g @dishanqian/cn2tw
-cn2tw P:\anqi\Desktop\tech\js\_test\js_node_cn2tw\zh_cn.txt P:\anqi\Desktop\tech\js\_test\js_node_cn2tw\zh_tw.txt
- * ```
+cn2tw zh_cn.txt zh_tw.txt
+```
  *
- * Usage: ```
-node index.js P:\anqi\Desktop\tech\js\_test\js_node_cn2tw\zh_cn.txt P:\anqi\Desktop\tech\js\_test\js_node_cn2tw\zh_tw.txt
- * ```
+ * Usage: ```bash
+git clone git+ssh://git@github.com/anqisoft/js_node_cn2tw
+cd js_node_cn2tw
+npm i @dishanqian/cn_and_tw
+node index.js zh_cn.txt zh_tw.txt
+```
  *
  * <en_us>
  * Created on Wed Jan 10 2024 20:09:00
@@ -31,6 +35,7 @@ exports.convert = void 0;
 
 const fs = require('fs');
 const { cn2tw } = require('@dishanqian/cn_and_tw');
+
 /**
  * <en_us>Convert Simplified Chinese to Traditional Chinese in a file</en_us>
  * <zh_cn>转换文件中的简体中文到繁体中文</zh_cn>
@@ -56,10 +61,56 @@ function convert(sourceFilename, goalFilename) {
     }
 }
 exports.convert = convert;
+
 (() => {
-    const { argv } = require('node:process');
+    const { argv, exit } = require('node:process');
+    const args = argv.slice(2);
+    let needShowTips = false;
+    args.forEach(arg => {
+        switch(arg.toLowerCase()) {
+            case '/help':
+            case '/h':
+            case '/?':
+
+            case '-help':
+            case '-h':
+            case '-?':
+
+            case '--help':
+            case '--h':
+            case '--?':
+                needShowTips = true;
+                break;
+            case '--v':
+            case '--version':
+            case '-v':
+            case '-version':
+            case '/v':
+            case '/version':
+                console.log('0.0.4');
+                exit();
+                break;
+            default:
+                break;
+        }
+    });
+    if(args.length < 2 || needShowTips) {
+        console.log('Copyright (c) 2024 anqisoft@gmail.com\n\
+            Translate file content from Simplified Chinese to Traditional Chinese.\n\n\
+            usage:\n\
+            cn2tw sourceFilename goalFilename\n\
+            eg:\n\
+            cn2tw zh_cn.txt zh_tw.txt\n\n\
+            show this tips:\n\
+            cn2tw /?|/h|/help|-?|-h|-help|--?|--h|--help\n\
+            show version:\n\
+            cn2tw /v|/version|-v|-version|--v|--version\n\
+        ');
+        exit();
+    }
+
     const START_DATE = new Date();
-    const [source, goal] = argv.slice(2);
+    const [source, goal] = args;
     console.log(convert(source, goal));
     const END_DATE = new Date();
     const USED_MILLISECONDS = END_DATE.getTime() - START_DATE.getTime();
